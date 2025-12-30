43bc6cc7be67b46aa351a5609ec20e54ba42793a
Sugan-g <suganvasu1993@gmail.com>
2025-12-30 14:35:11 +0530

    changes to be done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './components/Home';
 import ForgotPassword from './components/ForgotPassword';
 import ResetPassword from './components/ResetPassword';
 
