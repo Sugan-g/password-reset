223a15815e20f8affbbc3aa011310c43cd84c457
Sugan-g <suganvasu1993@gmail.com>
2026-01-02 13:48:23 +0530

    changes done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import ForgotPassword from './pages/ForgotPassword';
@@ -6,12 +6,20 @@ import ResetPassword from './pages/ResetPassword';
 import Login from './pages/Login';
 
 function App() {
+  const location = useLocation();
+
+  const hideNavbarRoutes = ['/login', '/forgot-password', '/reset-password'];
+  const hideNavbar = hideNavbarRoutes.some(route =>
+    location.pathname.startsWith(route)
+  );
+
   return (
     <>
-      <Navbar />
+      {!hideNavbar && <Navbar />}
+
       <Routes>
         <Route path="/" element={<Home />} />
-         <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
       </Routes>
